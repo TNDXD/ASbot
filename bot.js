a8a62f0babc3a5f8db184a6d1798de7508de2f45
TNDXD <48101705+TNDXD@users.noreply.github.com>
2019-06-09 20:49:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -545,7 +545,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers `);
     console.log(`---------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`سيرفر السيف العربي للحياة ولواقعية`,"http://twitch.tv/y04zgamer")
+  client.user.setGame(`سيرفر السيف العربي للحياة الواقعية`,"http://twitch.tv/y04zgamer")
    client.user.setStatus("dnd")
 });
 
